da5dcbd3abd1c56a003a08bb5e97939ee54a5328
Marcus Vinicius <106827268+DevMarcusVinicius@users.noreply.github.com>
2022-08-15 12:16:44 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+ var habumrguer = document.querySelector(".habumrguer");
+
+ habumrguer.addEventListener("click", function(){
+      document.querySelector(".sidebar").classList.toggle("showmenu");
+    
+ });
